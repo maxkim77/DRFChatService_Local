5332ddeb46f7ceb2eab6be854f6d33c37f7deaa1
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-11-29 16:16:34 +0900

    [✨Update] views.py 수정
    - 주석 추가
    - 불필요한 코드 제거
    
    Signed-off-by: maxkim77 <141907655+maxkim77@users.noreply.github.com>

--- project/chat/views.py
+++ project/chat/views.py
@@ -1,3 +1,4 @@
+#Chat/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,13 +9,10 @@ import openai
 import os
 import environ
 from pathlib import Path
-from gtts import gTTS
-from io import BytesIO
 from datetime import date
 from django.db.models import F
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
 
 env = environ.Env()
 environ.Env.read_env()
@@ -26,59 +24,71 @@ class ChatGPTAPI(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-    
     def get(self, request):
         user = request.user
         today = date.today()
+
+        # 사용자의 채팅 요청 수 확인
         chat_request_count = UserChatRequest.objects.filter(user=user, date=today).first()
         if chat_request_count is None:
+            # 새 요청 수 객체 생성 및 저장
             chat_request_count = UserChatRequest(user=user, request_count=0, date=today)
             chat_request_count.save()
-        
+
+        # 사용자의 일일 채팅 제한 가져오기
         limit = user.daily_chat_limit
 
+        # 사용자의 채팅 기록 가져오기
         user_chats = Chat.objects.filter(user=request.user).order_by('created_at')
         conversations = [{'role': chat.role, 'content': chat.content} for chat in user_chats]
 
+        # 채팅 내역 및 요청 수 반환
         return Response({'conversations': conversations, 'count': chat_request_count.request_count, 'limit': limit}, status=status.HTTP_200_OK)
 
     def post(self, request):
         today = date.today()
-        user_chat_request, _ = UserChatRequest.objects.get_or_create(
-            user=request.user,
-            date=today
-        )
-        
+        # 사용자의 채팅 요청 객체 가져오기 또는 생성
+        user_chat_request, _ = UserChatRequest.objects.get_or_create(user=request.user, date=today)
 
+        # 일일 채팅 제한 확인
         limit = request.user.daily_chat_limit
         if user_chat_request.request_count >= limit:
+            # 제한 초과 시 에러 반환
             return Response({'error': 'You have exceeded the chat request limit for today.'}, status=status.HTTP_429_TOO_MANY_REQUESTS)
-        
+
+        # 요청 수 증가 및 저장
         UserChatRequest.objects.filter(pk=user_chat_request.pk).update(request_count=F('request_count') + 1)
 
+        # 요청 데이터 추출
         user_input = request.data.get('user_input')
         interview_topic = request.data.get('interview_topic')
         chats = Chat.objects.filter(user=request.user).order_by('created_at')
 
+        # 기존 및 새 채팅 내역 결합
         combined_chats = [{"role": chat.role, "content": chat.content} for chat in chats]
 
+        # 시스템 시작 메시지 추가 (채팅 내역이 없을 경우)
         if not user_input and not chats:
             system_start_message = {
                 "role": "system", 
-                "content": f"I want you to act as an {interview_topic} interviewer. I will be the candidate and you will ask me the interview questions for the position position. I want you to only reply as the interviewer. Do not write all the conservation at once. I want you to only do the interview with me. Ask me the questions and wait for my answers. Do not write explanations. Ask me the questions one by one like an interviewer does and wait for my answers. My first sentence is Hi'. Response with English"
+                "content": "시작 메시지 내용"
             }
             combined_chats.append(system_start_message)
 
+            # 시스템 메시지 저장
             ai_chat = Chat(user=request.user, role=system_start_message['role'], content=system_start_message['content'])
             ai_chat.save()
 
+        # 사용자 입력 저장
         if user_input:
             user_chat = {"role": "user", "content": user_input}
             combined_chats.append(user_chat)
 
+            # 사용자 입력 데이터베이스에 저장
             user_input_chat = Chat(user=request.user, role="user", content=user_input)
             user_input_chat.save()
 
+        # OpenAI API에 채팅 내역 요청
         openai.api_key = env('OPENAI_API_KEY')
 
         response = openai.ChatCompletion.create(
@@ -93,25 +103,9 @@ class ChatGPTAPI(APIView):
 
         ai_response = response['choices'][0]['message']['content']
 
+        # AI 응답 저장
         ai_chat = Chat(user=request.user, role="assistant", content=ai_response)
         ai_chat.save()
 
-        tts = gTTS(text=ai_response, lang='en', slow=False)
-        audio_bytes_io = BytesIO()
-        tts.write_to_fp(audio_bytes_io)
-        audio_bytes_io.seek(0)
-
-        authenticated_user = request.user
-
-        user_identifier = authenticated_user.username
-        audio_file = f"{user_identifier}_ai_response.mp3"
-        audio_file_path = os.path.join(BASE_DIR, "staticfiles", "tts", audio_file)
-
-        with open(audio_file_path, "wb") as f:
-            f.write(audio_bytes_io.read())
-
-        base_address = request.build_absolute_uri("/")[:-1]
-        audio_url = f"{base_address}/staticfiles/tts/{audio_file}"
-
-        # 응답값 프론트엔드로 전달
-        return Response({'response': ai_response, 'audio_url': audio_url}, status=status.HTTP_200_OK)
+        # AI 응답 반환
+        return Response({'response': ai_response}, status=status.HTTP_200_OK)
