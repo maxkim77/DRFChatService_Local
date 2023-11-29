4585a10524365f093a690f6feb995540e52b9797
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-11-29 16:35:01 +0900

    Update tests.py
    Signed-off-by: maxkim77 <141907655+maxkim77@users.noreply.github.com>

--- project/accounts/tests.py
+++ project/accounts/tests.py
@@ -12,8 +12,7 @@ class SignupTestCase(APITestCase):
         }
         response = self.client.post(reverse('rest_register'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # 추가적인 검증이 필요할 수 있습니다.
-
+    
 class LoginTestCase(APITestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(email='test@example.com', password='somepassword')
@@ -23,24 +22,31 @@ class LoginTestCase(APITestCase):
             'email': 'test@example.com',
             'password': 'somepassword',
         }
-        response = self.client.post(reverse('rest_login'), data)  # URL 이름 수정
+        response = self.client.post(reverse('logout')) 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 class LogoutTestCase(APITestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(email='test@example.com', password='somepassword')
-        # 로그인 후 토큰 획득
-        response = self.client.post(reverse('dj_rest_auth:login'), {'email': 'test@example.com', 'password': 'somepassword'})
-        self.refresh_token = response.data['refresh_token']
+        self.client.login(email='test@example.com', password='somepassword')
 
     def test_logout(self):
-        response = self.client.post(reverse('rest_logout'), {'refresh': self.refresh_token})  # URL 이름 수정
+        response = self.client.post(reverse('rest_logout'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 class MyPageTestCase(APITestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(email='test@example.com', password='somepassword')
-        # 로그인 요청 후 토큰을 얻는다면 이를 사용
+        self.client.login(email='test@example.com', password='somepassword')  
+
+    def test_mypage(self):
+        response = self.client.get(reverse('mypage'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], 'test@example.com')
+    
+class MyPageTestCase(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(email='test@example.com', password='somepassword')
         response = self.client.post(reverse('dj_rest_auth:login'), {'email': 'test@example.com', 'password': 'somepassword'})
         self.token = response.data['access_token']
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
@@ -49,3 +55,20 @@ class MyPageTestCase(APITestCase):
         response = self.client.get(reverse('mypage'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['email'], 'test@example.com')
+
+    """
+    회원가입 기능에 대한 테스트 케이스
+    - test_signup: 새 사용자를 생성하는 POST 요청을 보내고, HTTP 201 Created 상태 코드로 응답하는지 확인
+    
+    로그인 기능에 대한 테스트 케이스
+    - setUp: 테스트를 위해 새 사용자 생성
+    - test_login: 생성된 사용자로 로그인하는 POST 요청을 보내고, HTTP 200 OK 상태 코드로 응답하는지 확인
+    
+    로그아웃 기능에 대한 테스트 케이스
+    - setUp: 테스트를 위해 새 사용자 생성 및 로그인 후 토큰 획득
+    - test_logout: 획득한 토큰을 사용해 로그아웃하는 POST 요청을 보내고, HTTP 200 OK 상태 코드로 응답하는지 확인
+    
+    마이페이지 기능에 대한 테스트 케이스
+    - setUp: 테스트를 위해 새 사용자 생성 및 로그인 후 토큰 획득, 테스트 클라이언트에 인증 토큰 설정
+    - test_mypage: 마이페이지 정보 조회 GET 요청을 보내고, HTTP 200 OK 상태 코드와 사용자 이메일 정보를 응답하는지 확인
+    """
