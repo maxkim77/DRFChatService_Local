91a9f5532e96fdde8b285af2a4a073eab6229f9a
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-11-30 22:02:27 +0900

    [🧨Update] urls.py
    - Swegger 문서 추가
    
    Signed-off-by: maxkim77 <141907655+maxkim77@users.noreply.github.com>

--- project/project/urls.py
+++ project/project/urls.py
@@ -4,10 +4,31 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import re_path
 from django.views.static import serve
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="DRF API",
+      default_version='v1',
+      description="API documentation for DRF Project",
+      terms_of_service="https://maxkim.github.io/",
+      contact=openapi.Contact(email="maxkim7723@yourproject.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include("accounts.urls")),
     path('api/', include('chat.urls')),  
     re_path(r'^static/(?:.*)$', serve, {'document_root': settings.STATIC_ROOT, }),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
