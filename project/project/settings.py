66d9abfb50ec69b3cabf62cc6f6c408dab00d052
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-12-01 15:35:36 +0900

    [✨Update] 환경변수 업데이트
    settings.py 19번째줄 환경변수 업데이트 
    
    Signed-off-by: maxkim77 <141907655+maxkim77@users.noreply.github.com>

--- project/project/settings.py
+++ project/project/settings.py
@@ -14,7 +14,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ha-31(fxb+9$rv3f_-fb@ck2^3ns(e89&&&bge_9=o@yi(6ill'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
